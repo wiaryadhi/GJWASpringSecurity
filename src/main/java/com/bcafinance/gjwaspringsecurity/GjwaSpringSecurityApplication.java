e432ce93bbf069421dbc8a44ca47334bfdc35d49
ARY <widhiarya@outlook.com>
2022-12-08 09:56:59 +0700

    Initial commit

---
+++ src/main/java/com/bcafinance/gjwaspringsecurity/GjwaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bcafinance.gjwaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjwaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjwaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcafinance/gjwaspringsecurity/GjwaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcafinance.gjwaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjwaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
